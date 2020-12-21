ca6809c3c04665233ab0951e4e063c2789a1f34a
SLBB2021 <76451541+SLBB2021@users.noreply.github.com>
2020-12-21 18:33:15 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("sully's webpage").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  welcome to karines group! join please!
+</div>
+`;
+
+// test comment
